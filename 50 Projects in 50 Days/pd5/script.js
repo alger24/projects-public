36460baa5c8c13a0687bd26b9f067d8de1c9ed79
Alger Bedaña <69410170+alger24@users.noreply.github.com>
2022-12-28 21:23:04 +0800

    Added pd5

---
+++ 50 Projects in 50 Days/pd5/script.js
@@ -0,0 +1,23 @@
+const loadText = document.querySelector('.loading-text');
+const bg = document.querySelector('.bg');
+
+let load = 0;
+
+let int = setInterval(blurring, 30);
+
+function blurring() {
+    load++
+
+    if(load > 99) {
+        clearInterval(int)
+    }
+
+    loadText.innerText = `${load}%`
+    loadText.style.opacity = scale(load, 0, 100, 1, 0);
+};
+
+const scale = (num, in_nim, in_max, out_min, out_max) => {
+    return (num - in_nim) * (out_max - out_min) / (in_max - in_min) + out_min
+};
+
+console.log(loadText);
