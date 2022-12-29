2bc0c9b40be36045fff0ca7e1df7186fe77a59dc
Alger Bedaña <69410170+alger24@users.noreply.github.com>
2022-12-29 10:11:54 +0800

    finished pd5

--- 50 Projects in 50 Days/pd5/script.js
+++ 50 Projects in 50 Days/pd5/script.js
@@ -1,4 +1,4 @@
-const loadText = document.querySelector('.loading-text');
+const loadingText = document.querySelector('.loading-text');
 const bg = document.querySelector('.bg');
 
 let load = 0;
@@ -6,18 +6,19 @@ let load = 0;
 let int = setInterval(blurring, 30);
 
 function blurring() {
-    load++
+    load++;
 
     if(load > 99) {
-        clearInterval(int)
+        clearInterval(int);
     }
 
-    loadText.innerText = `${load}%`
-    loadText.style.opacity = scale(load, 0, 100, 1, 0);
-};
+    loadingText.innerText = `${load}%`
+    loadingText.style.opacity = scale(load, 0, 100, 1, 0)
+    bg.style.filter = `blur(${scale(load, 0, 100, 30, 0)}px)`
+}
 
-const scale = (num, in_nim, in_max, out_min, out_max) => {
-    return (num - in_nim) * (out_max - out_min) / (in_max - in_min) + out_min
-};
 
-console.log(loadText);
+//https://stackoverflow.com/questions/10756313/javascript-jquery-map-a-range-of-numbers-to-another-range-of-numbers
+function scale (number, inMin, inMax, outMin, outMax) {
+    return (number - inMin) * (outMax - outMin) / (inMax - inMin) + outMin;
+}
