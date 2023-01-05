c68213b92d62702e46c0c01227f50bac2ebbb988
Alger Bedaña <69410170+alger24@users.noreply.github.com>
2023-01-05 19:06:30 +0800

    add&done pd12

---
+++ 50 Projects in 50 Days/pd12/script.js
@@ -0,0 +1,11 @@
+const toggles = document.querySelectorAll('.faq-toggle');
+
+toggles.forEach(toggle => {
+    toggle.addEventListener('click', () => {
+        toggle.parentNode.classList.toggle('active');
+    });
+});
+
+async function clog(data) {
+    return console.log(data);
+}
