e096449e10bb2f40bc27fec9a22a4f1e9f5114e2
Alger Bedaña <69410170+alger24@users.noreply.github.com>
2023-01-10 20:50:04 +0800

    pd16 done

--- 50 Projects in 50 Days/pd16/script.js
+++ 50 Projects in 50 Days/pd16/script.js
@@ -1,5 +1,5 @@
 const smallCups = document.querySelectorAll('.cup-small')
-const liters = document.getElementById('liters')
+const liters = document.querySelector('.liters')
 const percentage = document.getElementById('percentage')
 const remained = document.getElementById('remained')
 
@@ -28,4 +28,21 @@ function highlightCups(idx) {
 function updateBigCup() {
     const fullCups = document.querySelectorAll('.cup-small.full').length
     const totalCups = smallCups.length
+
+    if(fullCups === 0) {
+        percentage.style.visibility = 'hidden'
+        percentage.style.height = 0
+    } else {
+        percentage.style.visibility = 'visible'
+        percentage.style.height = `${fullCups / totalCups * 330}px`
+        percentage.innerText = `${fullCups / totalCups * 100}%`
+    }
+
+    if(fullCups === totalCups) {
+        remained.style.visibility = 'hidden'    
+        remained.style.height = 0
+    } else {
+        remained.style.visibility = 'visible'
+        liters.innerText = `${2-(250 * fullCups / 1000)}L`
+    }
 }
